9891eb6cedce1020acceff8ede818b0106da8e2c
saranraj.shri <shrisaranraj2001@gmail.com>
2020-05-07 00:59:01 -0400

    Admin DashBoard Added

---
+++ client/src/pages/Admin/DashBoard/Home.js
@@ -0,0 +1,74 @@
+import React from "react";
+
+// Style sheet
+import "./Home.css";
+
+// Bootstarp components
+import { Row, Col, Nav, Tab } from "react-bootstrap";
+
+// Stylesheets
+import "./Home.css";
+
+// FontAwesome
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faPlusCircle,
+  faChartBar,
+  faBoxOpen,
+  faStreetView,
+  faMoneyBillAlt,
+  faInfoCircle,
+  faShoppingCart,
+  faUserTie,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Components
+import Header from "./components/Header/Header";
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Header />
+        <Tab.Container id="left-tabs-example" defaultActiveKey="orders">
+          <div className="sidenav">
+            <div className="sidenav-inner">
+              <h5 className="mb-3">NAVIGATION</h5>
+              <Nav variant="pills" className="flex-column nav-links">
+                <Nav.Item>
+                  <Nav.Link eventKey="overview" className="inactive-link">
+                    <FontAwesomeIcon
+                      icon={faInfoCircle}
+                      className="tabIcon mr-2"
+                    />
+                    Overview
+                  </Nav.Link>
+                </Nav.Item>
+                <Nav.Item>
+                  <Nav.Link eventKey="por" className="inactive-link">
+                    <FontAwesomeIcon
+                      icon={faUserTie}
+                      className="tabIcon mr-2"
+                    />
+                    Post a new role
+                  </Nav.Link>
+                  <Nav.Item>
+                    <Nav.Link eventKey="d" className="inactive-link">
+                      <FontAwesomeIcon
+                        icon={faChartBar}
+                        className="tabIcon mr-2"
+                      />
+                      Manage Posts
+                    </Nav.Link>
+                  </Nav.Item>
+                </Nav.Item>
+              </Nav>
+            </div>
+          </div>
+        </Tab.Container>
+      </div>
+    );
+  }
+}
+
+export default Home;
---
+++ client/src/pages/Admin/DashBoard/components/Header/Header.js
@@ -0,0 +1,47 @@
+import React from "react";
+
+// Bootstrap components
+import { Navbar, Form } from "react-bootstrap";
+
+// Stylesheet
+import "./Header.css";
+
+// FontAwesome Icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faSearch } from "@fortawesome/free-solid-svg-icons";
+
+class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar className="navbar" sticky="top">
+          <div className="navbar-bg-light">
+            <h5 className="brandName">HIRE.ME</h5>
+          </div>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+
+          {/* Right Side content */}
+          <Navbar.Collapse
+            className="justify-content-end mr-5"
+            id="responsive-navbar-nav"
+          >
+            <div>
+              <FontAwesomeIcon icon={faSearch} className="inputIcon" />
+              <Form.Control
+                type="search"
+                className="searchBar mr-4"
+                placeholder="Search"
+              ></Form.Control>
+            </div>
+            <h5 className="recruiterName mr-3">Recruiter Name</h5>
+            <FontAwesomeIcon icon={faBars} className="icon" />
+          </Navbar.Collapse>
+
+          {/* <Navbar.Brand href="#home"></Navbar.Brand> */}
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default Header;
